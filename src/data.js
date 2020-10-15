7729a598f50e39b702026c8cd0beac570672b5a2
Lisa M <jlmutheu@gmail.com>
2020-10-15 22:47:14 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,134 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Chanel 1 - red",
+        img: 'img/redbag.jpg',
+        price: 2000,
+        company: "Chanel",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+
+    },
+
+    {
+        id: 2,
+        title: "Gucci - brown",
+        img: 'img/brownbag.jpg',
+        price: 3000,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 3,
+        title: "Gucci - pink",
+        img: 'img/pinkbag.jpg',
+        price: 2500,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Chanel 1 - red",
+        img: 'img/redbag.jpg',
+        price: 2000,
+        company: "Chanel",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+
+    },
+
+    {
+        id: 5,
+        title: "Gucci - brown",
+        img: 'img/brownbag.jpg',
+        price: 3000,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 6,
+        title: "Gucci - pink",
+        img: 'img/pinkbag.jpg',
+        price: 2500,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Chanel 1 - red",
+        img: 'img/redbag.jpg',
+        price: 2000,
+        company: "Chanel",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+
+    },
+
+    {
+        id: 8,
+        title: "Gucci - brown",
+        img: 'img/brownbag.jpg',
+        price: 3000,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 9,
+        title: "Gucci - pink",
+        img: 'img/pinkbag.jpg',
+        price: 2500,
+        company: "Gucci",
+        info:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+
+];
+
+export const detailProduct = {
+    id:1,
+    title: "Chanel 1 - Red",
+    img: 'img/redbag.jpg',
+    price: 2000,
+    company:'Chanel',
+    info: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  lore magna aliqua. Ac ut consequat semper viverra nam. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices.  Rhoncus est pellentesque elit ullamcorper dignissim crastincidunt lobortis.",
+    inCart: false,
+    count: 0,
+    total: 0
+
+};
